f03570962656c28b44377f626e191fcbf7c7f5a2
YOGA\qweqw <rusli.kru2@gmial.com>
2023-10-10 16:43:11 +0300

    Добавил текст, для криков, нужно затестить. немного прибавил отображение крови в глазках, при ранении. Нужно затестить на шлеме Плюс тут же статистика в таверне Плюс тут же камеры в последней карте

---
+++ Assets/_App/Scripts/Player/VoiceText.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+
+    public class VoiceText : MonoBehaviour
+    {
+        public GameObject textKrik;  // Объект, состояние которого нужно синхронизировать
+
+        void OnEnable()
+        {
+            // Включаем связанный объект, когда объект, содержащий этот скрипт, включается
+            if (textKrik != null)
+            {
+                textKrik.SetActive(true);
+            }
+        }
+
+        void OnDisable()
+        {
+            // Выключаем связанный объект, когда объект, содержащий этот скрипт, выключается
+            if (textKrik!= null)
+            {
+                textKrik.SetActive(false);
+            }
+        }
+    }
