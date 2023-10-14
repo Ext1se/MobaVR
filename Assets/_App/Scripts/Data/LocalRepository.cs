4d8cf65bc6647ae06d91159d012f39250469234b
Igor Ponomarev <felislord@gmail.com>
2023-10-14 15:11:34 +0300

    Add backup data

--- Assets/_App/Scripts/Data/LocalRepository.cs
+++ Assets/_App/Scripts/Data/LocalRepository.cs
@@ -1,16 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using MobaVR;
 using UnityEngine;
 
 public class LocalRepository
 {
     public const string SERVER_MODE = "SERVER_MODE";
     public const string LAST_IP_ADDRESS = "LastIPAddress";
+    public const string BACKUP_PLAYER = "BACKUP_PLAYER";
     
     public void SetLocalServer(bool isLocalServer)
     {
         PlayerPrefs.SetInt(SERVER_MODE, isLocalServer ? 0 : 1);
         PlayerPrefs.Save();
+    }
+    
+    public void SavePlayerData(BackupPlayerData backupPlayerData)
+    {
+        backupPlayerData.BackupDate = DateTime.Now.Millisecond;
+        string json = JsonUtility.ToJson(backupPlayerData);
+        PlayerPrefs.SetString(BACKUP_PLAYER, json);
+        PlayerPrefs.Save();
+    }
+
+    public bool TryGetBackupPlayerData(out BackupPlayerData backupPlayerData)
+    {
+        string json = PlayerPrefs.GetString(BACKUP_PLAYER, "");
+        if (string.IsNullOrEmpty(json))
+        {
+            backupPlayerData = null;
+            return false;
+        }
+
+        backupPlayerData = JsonUtility.FromJson<BackupPlayerData>(json);
+        return true;
     }
 
     public bool IsLocalServer => PlayerPrefs.GetInt(SERVER_MODE, 0) == 0;
--- Assets/_App/Scripts/Statistics/GameStatistics.cs
+++ Assets/_App/Scripts/Statistics/GameStatistics.cs
@@ -1,12 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
 
 namespace MobaVR
 {
-    public class GameStatistics : MonoBehaviourPun
+    public class GameStatistics : MonoBehaviourPunCallbacks
     {
+        [SerializeField] private bool IsBackupData = true;
+        
         private ClassicGameSession m_GameSession;
 
+        private LocalRepository m_LocalRepository;
         private List<PlayerVR> m_Players = new();
 
         private void OnEnable()
@@ -30,6 +36,7 @@ namespace MobaVR
         private void Awake()
         {
             m_GameSession = FindObjectOfType<ClassicGameSession>();
+            m_LocalRepository = new LocalRepository();
         }
 
         public void SendDeathData(DeathPlayerData deathPlayerData)
@@ -84,5 +91,48 @@ namespace MobaVR
         {
             m_Players.Remove(playerVR);
         }
+
+        #region Photon
+
+        private void SavePlayerData()
+        {
+            if (!IsBackupData)
+            {
+                return;
+            }
+            
+            if (m_GameSession == null)
+            {
+                return;
+            }
+
+            if (m_GameSession.LocalPlayer == null)
+            {
+                return;
+            }
+
+            BackupPlayerData backupPlayerData = new BackupPlayerData
+            {
+                PlayerData = m_GameSession.LocalPlayer.PlayerData,
+                PlayerScoreData = m_GameSession.LocalPlayer.PlayerScore.ScoreData,
+                BackupDate = DateTime.Now.Ticks
+            };
+
+            m_LocalRepository.SavePlayerData(backupPlayerData);
+        }
+        
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            SavePlayerData();
+            base.OnDisconnected(cause);
+        }
+
+        public override void OnLeftRoom()
+        {
+            SavePlayerData();
+            base.OnLeftRoom();
+        }
+
+        #endregion
     }
 }
