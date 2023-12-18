5f4819c1dcfe8063e7b62ffaac3ccf465215c17c
YOGA\qweqw <rusli.kru2@gmial.com>
2023-12-18 16:40:04 +0300

    Доработка обучения. Этап: книа.
     + Чуть короче первую вступительную фразу
     + Нужно, чтобы они не болтали вдвоём, при входе в книгу
     + Посмотреть, что с кнопкой далее
     + включаем следующий лист ввода имени, и у нас должен исчезать персонаж
     + при каждом перелистывании у нас должен исчезать скин
     + несколько раз нельзя нажимать
     + Вышел и вошёл, и задвоилась страница
     + Нужно поднять точку стрелки в первой точке
     + Нужно, чтобы исчезала стрелка, при подходе
     + Нужно скрыть и показать книгу с подсказками, при нажатии на X (добавли книгу в InputVR в левую руку, и повесил скрипт ShowHints на LeftController)
     + Завершить обучение книги, перейдя в тир

--- Assets/_App/Scripts/Obuchenie/NEW_obuchenie/ActiveSkinsBook.cs
+++ Assets/_App/Scripts/Obuchenie/NEW_obuchenie/ActiveSkinsBook.cs
@@ -2,21 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// скипт, который включает или выключает скины у книги. Висит в двух страницах. на первой - он включает, при включении первой страницы, на вводе имени, он выключает.
+// скипт, который включает или выключает скины игроков у книги. Висит на страницах с информацией о скинах и выключается кнопками далее или назад
 
 public class ActiveSkinsBook : MonoBehaviour
 {
     public GameObject targetObject; // Объект, который будет активироваться или деактивироваться
-    public bool activateOnEnable; // Булева переменная, которая определяет, активировать или деактивировать объект
+
+   
 
     void OnEnable()
     {
-        // Проверяем, нужно ли активировать или деактивировать объект
+
+            if (targetObject != null) // Убедитесь, что ссылка на объект задана
+            {
+                targetObject.SetActive(true); // Активируем  объект 
+            }
+
+    }       
+
+
+
+    public void activateskins()
+    {
+      
         if (targetObject != null) // Убедитесь, что ссылка на объект задана
         {
-            targetObject.SetActive(
-                activateOnEnable); // Активируем или деактивируем объект в зависимости от значения activateOnEnable
+            targetObject.SetActive(false); // деактивируем объект 
         }
+        
     }
 
 }
