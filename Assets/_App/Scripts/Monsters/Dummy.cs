300d215fc07a1db77dbf52329660810133a5d50f
Igor Ponomarev <felislord@gmail.com>
2023-12-22 21:00:23 +0300

    Add Dummy

---
+++ Assets/_App/Scripts/Monsters/Dummy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace MobaVR
+{
+    public class Dummy : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem m_HealVFX;
+        [SerializeField] private ParticleSystem m_FireVFX;
+        [SerializeField] private ParticleSystem m_FogVFX;
+        
+        public void ShowFog()
+        {
+            
+        }
+
+        public void ShowFire()
+        {
+            
+        }
+
+        public void ShowHeal()
+        {
+            
+        }
+    }
+}
