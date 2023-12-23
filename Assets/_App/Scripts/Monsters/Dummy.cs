10e6a75251e8fa63f3ad8a78e79a7d29e111726b
Igor Ponomarev <felislord@gmail.com>
2023-12-23 13:04:36 +0300

    Update scenes and add Dummy

--- Assets/_App/Scripts/Monsters/Dummy.cs
+++ Assets/_App/Scripts/Monsters/Dummy.cs
@@ -1,26 +1,62 @@
-﻿using UnityEngine;
+﻿using System;
+using DamageNumbersPro;
+using UnityEngine;
 
 namespace MobaVR
 {
     public class Dummy : MonoBehaviour
     {
+        [SerializeField] private DamageNumberView m_DamageNumber;
+
         [SerializeField] private ParticleSystem m_HealVFX;
         [SerializeField] private ParticleSystem m_FireVFX;
         [SerializeField] private ParticleSystem m_FogVFX;
-        
+
+        private void Awake()
+        {
+            HideAllEffects();
+        }
+
+        private void HideAllEffects()
+        {
+            m_HealVFX.Stop();
+            m_FireVFX.Stop();
+            m_FogVFX.Stop();
+        }
+
         public void ShowFog()
         {
-            
+            HideAllEffects();
+            m_FogVFX.Play();
         }
 
         public void ShowFire()
         {
-            
+            HideAllEffects();
+            m_FireVFX.Play();
         }
 
         public void ShowHeal()
         {
-            
+            HideAllEffects();
+            m_HealVFX.Play();
+        }
+
+        public void ShowDamage(HitData hitData)
+        {
+            if (hitData.Action != HitActionType.Damage)
+            {
+                return;
+            }
+
+            m_DamageNumber.SpawnNumber(m_DamageNumber.transform.position,
+                                       hitData.Amount,
+                                       Monster.MonsterDamageType.HP);
+        }
+
+        public void Hit(HitData hitData)
+        {
+            ShowDamage(hitData);
         }
     }
 }
